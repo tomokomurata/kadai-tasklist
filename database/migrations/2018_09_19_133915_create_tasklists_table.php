2c17ceb92999c7adc303a9c817e01b51a7d9a56b
chico murata <qbic61+tmmt@gmail.com>
2018-09-21 07:38:23 +0000

    create_tasklists_table edit

---
+++ database/migrations/2018_09_19_133915_create_tasklists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasklists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');    // content カラム追加
+            $table->string('status', 10);    // content カラム追加課題用
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasklists');
+    }
+}
