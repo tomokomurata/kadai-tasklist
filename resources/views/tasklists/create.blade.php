f2714517a873867964ec4f22f44fc1680532731e
chico murata <qbic61+tmmt@gmail.com>
2018-09-21 08:00:53 +0000

    create.blade edit

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', '内容:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
 
