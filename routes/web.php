4d85d02eccf189d29f846ac42f034221a6abf12f
chico murata <qbic61+tmmt@gmail.com>
2018-09-25 09:53:02 +0000

    loginpage  add

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@
 Route::get('/', 'TasklistsController@index');
 
 Route::resource('tasklists', 'TasklistsController');
+
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 ;
